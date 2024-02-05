e7400c3afc52392c7435d2b2ab7ac21e4761589f
kanza707 <158589534+kanza707@users.noreply.github.com>
2024-02-04 22:35:00 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCnNmP6qK4bXrD9tAF3K4oUb4IhYtoQ8OM",
-    authDomain: "hackathon-75529.firebaseapp.com",
-    databaseURL: "https://hackathon-75529-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "hackathon-75529",
-    storageBucket: "hackathon-75529.appspot.com",
-    messagingSenderId: "950842692932",
-    appId: "1:950842692932:web:3221b6855ca894b2c0f311"
+  apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 export {firebaseConfig};
